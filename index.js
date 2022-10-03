c3244ee831c3b2194d804c2824ff95b7dd002d7b
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-10-03 12:33:47 -0400

    commit tree cb5f58d1f1e944d049fdc0c36a1c19c5af2b4e2c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+const drivers = [
+    {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+    {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+    {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+    {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+    {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+const findMatching = (name) => {
+    if (name === 'Bobby') {
+        return name
+    }
+}
+drivers.filter((driver) => {
+    if (driver.name === 'Bobby') {
+        return driver.name
+    }
+})
+
